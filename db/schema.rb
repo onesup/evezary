69c28fc16d7759c03f49111569eee262ed5dccbc
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-01-09 04:46:22 +0900

    이미지 변경 반영

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107073511) do
+ActiveRecord::Schema.define(version: 20140108172333) do
+
+  create_table "surveys", force: true do |t|
+    t.string   "title"
+    t.integer  "total"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
